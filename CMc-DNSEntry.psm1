b17e1d9854ff302d46ccc0e07916e82d0c00e330
ChaozMc <chaozmc@is-jo.org>
2020-02-19 00:17:42 +0100

    mass undo; accept pipe for undo, create backup folder if it doesnt exist

--- CMc-DNSEntry.psm1
+++ CMc-DNSEntry.psm1
@@ -46,7 +46,24 @@ function Set-DNSEntry {
     #Check plausability
     if ($RecordType -eq "A" -and (!$NewValue.Contains("."))) { Write-Error "Not a valid new Value" -ErrorAction Stop }
 
+    #As the BackupTarget is a Folder, check if it ends with \
+    if (-not $BackupDir.EndsWith("\")) { $BackupDir = "$($BackupDir)\" }
 
+    #Check if TargetFolder for Backup exists
+    if (-not (Test-Path -LiteralPath $BackupDir)) {
+        try {
+            New-Item -Path $BackupDir -ItemType Directory -ErrorAction Stop | Out-Null #-Force
+        }
+        catch {
+            Write-Error -Message "Unable to create directory $($BackupDir). Error was: $_" -ErrorAction Stop
+        }
+        Write-Verbose -Message "Successfully created directory $($BackupDir)"
+    }
+    else {
+        Write-Verbose -Message "Backup directory exists"
+    }
+
+    #If RecordType is A then check if NewValue is a valid IP address
     if ($RecordType -eq "A") 
     {
         try {[System.Net.IPAddress]::parse($NewValue)}
@@ -157,7 +174,24 @@ function Add-DNSEntry {
     #Check plausability
     if ($RecordType -eq "A" -and (!$NewValue.Contains("."))) { Write-Error "Not a valid new Value" -ErrorAction Stop }
     
+    #As the BackupTarget is a Folder, check if it ends with \
+    if (-not $BackupDir.EndsWith("\")) { $BackupDir = "$($BackupDir)\" }
 
+    #Check if TargetFolder for Backup exists
+    if (-not (Test-Path -LiteralPath $BackupDir)) {
+        try {
+            New-Item -Path $BackupDir -ItemType Directory -ErrorAction Stop | Out-Null #-Force
+        }
+        catch {
+            Write-Error -Message "Unable to create directory $($BackupDir). Error was: $_" -ErrorAction Stop
+        }
+        Write-Verbose -Message "Successfully created directory $($BackupDir)"
+    }
+    else {
+        Write-Verbose -Message "Backup directory exists"
+    }
+
+    #If RecordType is A check if the NewValue is a valid IP
     if ($RecordType -eq "A") 
     {
         Write-Verbose -Message "Check if IP-Address is in valid format"
@@ -204,39 +238,97 @@ function Undo-DNSEntry {
 
         .EXAMPLE
             Undo-DNSEntry -BackupFile "C:\Temp\host.example.com-yyyy-mm-dd_hh-mm-ss.xml"
+            Undo-DNSEntry -BackupFileSet (Get-ChildItem -Path C:\Temp -Filter "*.xml")
+            Get-ChildItem -Path C:\Temp -Filter "*.xml" | Undo-DNSEntry
     #>
 
-    [CmdletBinding(ConfirmImpact="Low")]
-       param(
-           #Specifies the file where the backup of one transaction is located
-           [Parameter(Mandatory)]
-           [string]$BackupFile
-       )
+        [CmdletBinding(ConfirmImpact="Low")]
+        param(
+            #Specifies the file where the backup of one transaction is located
+            [Parameter(Mandatory=$False, Position=0, ParameterSetName="BackupFile", ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+            [Alias('FullName')]
+            [PSCustomObject]$BackupFile,
 
-       Write-Verbose "Load backup-object"
-       $savedObject = Import-Clixml -Path $BackupFile
+            #Specifies a whole directory where backups exist so a range of actions can be undone
+            [Parameter(Mandatory=$False, Position=0, ParameterSetName="BackupFileSet")]
+            [object]$BackupFileSet
+        )
 
-       Write-Verbose -Message "Decide what needs to be reverted"
-       if ($savedObject.action -eq "AddEntry") {
-            $newEntry = $savedObject.newEntry
-            $TargetZone = $savedObject.DNSZone
-            $TargetDNSServer = $savedObject.DNSServer
-            Write-Verbose -Message "Object to delete: $($newEntry.HostName), DNSZone: $($TargetZone), DNS-Server: $($TargetDNSServer)"
-            Remove-DnsServerResourceRecord -InputObject $newEntry -ZoneName $TargetZone -ComputerName $TargetDNSServer -Force
-            Write-Verbose -Message "Removed object"
-            Get-Item -Path $BackupFile | Rename-Item -NewName { "[REVERTED]_$($_.Name)" }
-            Write-Verbose -Message "Renamed Backupfile"
-       } elseif ($savedObject.action -eq "ChangeEntry") {
-            $oldEntry = $savedObject.oldEntry
-            $newEntry = $savedObject.newEntry
-            $TargetZone = $savedObject.DNSZone
-            $TargetDNSServer = $savedObject.DNSServer
-            Write-Verbose -Message "Loaded objects from Backup"
-            Set-DnsServerResourceRecord -NewInputObject $oldEntry -OldInputObject $newEntry -ZoneName $TargetZone -ComputerName $TargetDNSServer
-            Write-Verbose -Message "Reverted in DNS Server"
-            Get-Item -Path $BackupFile | Rename-Item -NewName { "[REVERTED]_$($_.Name)" }
-            Write-Verbose -Message "Renamed BackupFile"
-        }
+        process {
+
+            if ($BackupFile) {
+
+                if ($BackupFile -is [System.IO.FileInfo] -and $BackupFile.FullName.EndsWith(".xml") -and -not $BackupFile.Name.StartsWith("[REVERTED]")) {
+    
+                    Write-Verbose "Load backup-object from piped object path string $($BackupFile.FullName)"
+                    $savedObject = Import-Clixml -Path $BackupFile.FullName
+        
+                    Write-Verbose -Message "Decide what needs to be reverted"
+                    if ($savedObject.action -eq "AddEntry") {
+                        $newEntry = $savedObject.newEntry
+                        $TargetZone = $savedObject.DNSZone
+                        $TargetDNSServer = $savedObject.DNSServer
+                        Write-Verbose -Message "Object to delete: $($newEntry.HostName), DNSZone: $($TargetZone), DNS-Server: $($TargetDNSServer)"
+                        Remove-DnsServerResourceRecord -InputObject $newEntry -ZoneName $TargetZone -ComputerName $TargetDNSServer -Force
+                        Write-Verbose -Message "Removed object"
+                        Get-Item -Path $BackupFile.FullName | Rename-Item -NewName { "[REVERTED]_$($_.Name)" }
+                        Write-Verbose -Message "Renamed Backupfile"
+                    } elseif ($savedObject.action -eq "ChangeEntry") {
+                        $oldEntry = $savedObject.oldEntry
+                        $newEntry = $savedObject.newEntry
+                        $TargetZone = $savedObject.DNSZone
+                        $TargetDNSServer = $savedObject.DNSServer
+                        Write-Verbose -Message "Loaded objects from Backup"
+                        Set-DnsServerResourceRecord -NewInputObject $oldEntry -OldInputObject $newEntry -ZoneName $TargetZone -ComputerName $TargetDNSServer
+                        Write-Verbose -Message "Reverted in DNS Server"
+                        Get-Item -Path $BackupFile.FullName | Rename-Item -NewName { "[REVERTED]_$($_.Name)" }
+                        Write-Verbose -Message "Renamed BackupFile"
+                    }
+    
+                } elseif ($BackupFile -is [string] -and $BackupFile.EndsWith(".xml") -and -not (Get-Item $BackupFile).Name.StartsWith("[REVERTED]")) {
+    
+                    Write-Verbose "Load backup-object from single path string $($BackupFile)"
+                    $savedObject = Import-Clixml -Path $BackupFile
+        
+                    Write-Verbose -Message "Decide what needs to be reverted"
+                    if ($savedObject.action -eq "AddEntry") {
+                        $newEntry = $savedObject.newEntry
+                        $TargetZone = $savedObject.DNSZone
+                        $TargetDNSServer = $savedObject.DNSServer
+                        Write-Verbose -Message "Object to delete: $($newEntry.HostName), DNSZone: $($TargetZone), DNS-Server: $($TargetDNSServer)"
+                        Remove-DnsServerResourceRecord -InputObject $newEntry -ZoneName $TargetZone -ComputerName $TargetDNSServer -Force
+                        Write-Verbose -Message "Removed object"
+                        Get-Item -Path $BackupFile | Rename-Item -NewName { "[REVERTED]_$($_.Name)" }
+                        Write-Verbose -Message "Renamed Backupfile"
+                    } elseif ($savedObject.action -eq "ChangeEntry") {
+                        $oldEntry = $savedObject.oldEntry
+                        $newEntry = $savedObject.newEntry
+                        $TargetZone = $savedObject.DNSZone
+                        $TargetDNSServer = $savedObject.DNSServer
+                        Write-Verbose -Message "Loaded objects from Backup"
+                        Set-DnsServerResourceRecord -NewInputObject $oldEntry -OldInputObject $newEntry -ZoneName $TargetZone -ComputerName $TargetDNSServer
+                        Write-Verbose -Message "Reverted in DNS Server"
+                        Get-Item -Path $BackupFile | Rename-Item -NewName { "[REVERTED]_$($_.Name)" }
+                        Write-Verbose -Message "Renamed BackupFile"
+                    }
+    
+                }
+
+            } elseif ($BackupFileSet) {
+    
+                foreach ($File in $BackupFileSet) {
+    
+                    if ($File -is [System.IO.FileInfo]) {
+
+                        Undo-DNSEntry -BackupFile $File.FullName
+                            
+                    }
+                    
+                }
+           }
+
+    }
+       
 }
 
 Export-ModuleMember -Function Set-DNSEntry
