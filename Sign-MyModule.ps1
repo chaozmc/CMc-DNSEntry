6d0dcbe28973bfaa1d682383b7173a7c2cba837f
ChaozMc <chaozm@is-jo.org>
2020-03-01 20:54:49 +0100

    added digital signature

---
+++ Sign-MyModule.ps1
@@ -0,0 +1,5 @@
+﻿$mycert = @(Get-ChildItem Cert:\CurrentUser\My -CodeSigningCert)[0]
+$modulefiles = Get-ChildItem -Recurse -Path ".\CMc-DNSEntry" -File
+$modulefiles | ForEach-Object { Set-AuthenticodeSignature -Certificate $mycert -TimestampServer http://timestamp.digicert.com -IncludeChain all -FilePath $_.FullName } 
+
+. '.\Publish-MyModule.ps1'
