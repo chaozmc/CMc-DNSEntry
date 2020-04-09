cbd3254e293f75df2c90c3a24f0ba8f91da97ee2
ChaozMc <chaozmc@is-jo.org>
2020-04-10 01:12:57 +0200

    prework for corerequest class

---
+++ CMc-DNSEntry/Public/New-DNSRequest.ps1
@@ -0,0 +1,68 @@
+function New-DNSRequest {
+
+    <#
+        .SYNOPSIS
+            Creates a new DNSRequest
+
+        .EXAMPLE
+            New-DNSRequest -Reference "REQ4711" -Requestor "sAMAccountName" -PathToClassDLL "C:\Temp\CoreRequest.dll" -WorkingDir "C:\Temp\Test"
+    #>
+
+    [CmdletBinding(ConfirmImpact="Low")]
+    [OutputType('CoreReques.DNSRequest')]
+    param(
+        # Name of the Reference of the Record
+        [Parameter(Mandatory)]
+        [string]$Reference,
+
+        #Requestor Name, preferable the sAMAccountName of a User
+        [Parameter(Mandatory)]
+        [string]$Requestor,
+        
+        #Path to the CoreRequest.dll File
+        [Parameter(Mandatory)]
+        [string]$PathToClassDLL,
+
+        #Path to the WorkingDirectory. There the Request will be saved
+        [Parameter(Mandatory)]
+        [string]$WorkingDir
+
+    )
+
+    begin {
+        if (-not (Test-Path -Path $PathToClassDLL) ) {
+            Write-Error -Message "Unable to find DLL-File Path($($PathToClassDLL)). Error was: $_" -ErrorAction Stop
+        }
+        try {
+            Add-Type -Path $PathToClassDLL
+        }
+        catch {
+            Write-Error -Message "Unable to load DLL-File Path($($PathToClassDLL)). Error was: $_" -ErrorAction Stop
+        }
+        Write-Verbose -Message "Successfully loaded DLL-File $($PathToClassDLL)"
+
+        if (-not (Test-Path -Path $WorkingDir) ) {
+            try {
+                New-Item -Path $WorkingDir -ItemType Directory -ErrorAction Stop | Out-Null #-Force
+            }
+            catch {
+                Write-Error -Message "Unable to create working directory $($WorkingDir). Error was: $_" -ErrorAction Stop
+            }
+            Write-Verbose -Message "Successfully created working directory $($WorkingDir)"
+        }
+        
+        
+    }
+
+    process {
+        $newRequest = New-Object -TypeName CoreRequest.DNSRequest
+
+        $newRequest.RequestReference = $Reference
+        $newRequest.requestorAccountName = $Requestor
+
+        $newRequest.SaveTo($WorkingDir)
+
+        return $newRequest
+    }
+
+}
